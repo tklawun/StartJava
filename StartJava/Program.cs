44d95c1dc33b497bde85d6e30cfddafff3c44232
tklawun <das70erjahr@hotmail.com>
2018-04-06 23:56:18 +0200

    Projektdateien hinzufügen.

---
+++ StartJava/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace StartJava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process javaProc = new Process();
+            javaProc.StartInfo.FileName()
+                }
+    }
+}
